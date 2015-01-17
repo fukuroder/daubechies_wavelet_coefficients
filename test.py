c5d9cfea0ed38f9b3fe4ef5b076935fbd77da811
fukuroda <fukuroder@live.jp>
2015-01-18 06:40:25 +0900

    修正

--- test.py
+++ test.py
@@ -3,53 +3,65 @@ import sympy
 import sympy.mpmath as sm
 import scipy.signal
 import matplotlib.pyplot as plt
-#import time
 
 # precition
 sm.mp.prec = 256
 
 def daubechis(N):
-	q_z = [sm.binomial(N-1+k,k) for k in reversed(range(N))]
-	q_sol = sm.mp.polyroots(q_z, maxsteps=200, extraprec=64)
+	# make polynomial
+	q_y = [sm.binomial(N-1+k,k) for k in reversed(range(N))]
 
-	roots = []
-	for q in q_sol:
-		roots += sm.mp.polyroots([sm.mpf('-1/4'), sm.mpf('1/2') - q, sm.mpf('-1/4')])
+	# get polynomial roots y[k]
+	y = sm.mp.polyroots(q_y, maxsteps=200, extraprec=64)
 
-	roots_in_unitcircle = [s for s in roots if sm.fabs(s) < 1]
+	z = []
+	for yk in y:
+		# subustitute y = -1/4z + 1/2 - 1/4/z to factor f(y) = y - y[k]
+		f = [sm.mpf('-1/4'), sm.mpf('1/2') - yk, sm.mpf('-1/4')]
 
-	h0z = 1
-	for s in roots_in_unitcircle:
-		h0z *= sympy.sympify('(z-s)/(1-s)').subs('s',s)
+		# get polynomial roots z[k]
+		z += sm.mp.polyroots(f)
 
+	# make polynomial using the roots within unit circle
+	h0z = sm.sqrt('2')
+	for zk in z:
+		if sm.fabs(zk) < 1:
+			h0z *= sympy.sympify('(z-zk)/(1-zk)').subs('zk',zk)
+
+	# adapt vanising moments
 	hz = (sympy.sympify('(1+z)/2')**N*h0z).expand()
 
-	scaling_coeff = [sympy.re(hz.coeff('z',k)) for k in reversed(range(N*2))]
+	# get scaling coefficients
+	return [sympy.re(hz.coeff('z',k)) for k in reversed(range(N*2))]
 
-	sqrt2 = sm.sqrt('2')
-	return [s*sqrt2 for s in scaling_coeff]
-
-if __name__ == '__main__':
+def main():
 	for N in range(2,100):
-		f = open('db' + str(N).zfill(2) +'_coefficients.txt', 'w')
-		#start_time = time.clock()
+		# get dbN coeffients
 		dbN = daubechis(N)
-		#print N, 'time:', time.clock() - start_time
+		
+		# write coeffients
+		f = open('db' + str(N).zfill(2) +'_coefficients.txt', 'w')
 		f.write('# db' + str(N) + ' scaling coefficients\n')
 		for i, c in enumerate(dbN):
 			f.write('h['+ str(i) + ']='+ sm.nstr(c,40) + '\n')
 		f.close()
 
+		# get an approximation of scaling function
 		x, phi, psi = scipy.signal.cascade(dbN)
 
+		# plot scaling function
 		plt.plot(x, phi, 'k')
 		plt.grid()
 		plt.title('db' + str(N) + ' scaling function')
 		plt.savefig('db' + str(N).zfill(2) + '_scaling' + '.png')
 		plt.clf()
 
+		# plot wavelet
 		plt.plot(x, psi, 'k')
 		plt.grid()
 		plt.title( 'db' + str(N) + " wavelet" )
 		plt.savefig('db' + str(N).zfill(2) + '_wavelet' + '.png')
 		plt.clf()
+
+if __name__ == '__main__':
+	main()
